b3ee79967c88820816d148bea9e52aef581f8871
anraku <anraku@daimorinoMacBook.local>
2018-05-10 01:34:08 +0900

    SQL文を修正

--- infra/mysql/sqls/1_init.sql
+++ infra/mysql/sqls/1_init.sql
@@ -4,6 +4,7 @@ use app;
 CREATE TABLE tasks (
   id int(11) unsigned not null auto_increment,
   text varchar(255) not null,
+  done tinyint(1) not null DEFAULT 0,
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp on update current_timestamp,
   primary key (id)
